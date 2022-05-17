50b55932a27bf3145dab0982e35b5fe5857cb728
root <root@80-78-254-16.cloudvps.regruhosting.ru>
2022-05-17 15:53:10 +0300

    assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,25 +6,103 @@ export default {
     port: 38907
   },
   head: {
-    title: 'fashiontime',
+    title: 'Время красоты',
+    htmlAttrs: {
+      lang: "ru"
+    },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "48x48",
+        href: "/favicon-48x48.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicon-96x96.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/android-icon-192x192.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "57x57",
+        href: "/apple-icon-57x57.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/apple-icon-60x60.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        href: "/apple-icon-72x72.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/apple-icon-76x76.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "114x114",
+        href: "/apple-icon-114x114.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        href: "/apple-icon-120x120.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        href: "/apple-icon-144x144.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        href: "/apple-icon-152x152.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-icon-180x180.png"
+      }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/fonts.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -32,8 +110,35 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
+
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          backgroundImage: {
+            'main-page': "url('@/assets/img/time-bg.jpg')"
+          },
+          fontFamily: {
+            'alice': ['Alice'],
+            'ametist': ['Ametist'],
+            'izis': ['Izis'],
+            'olympia': ['Olympia'],
+            'seminaria': ['Seminaria']
+          },
+          fontSize: {
+            '10xl': '10rem',
+            '11xl': '12rem',
+            '12xl': '15rem',
+            '13xl': '18rem',
+            '14xl': '21rem',
+            '15xl': '25rem'
+          }
+        }
+      }
+    }
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -41,6 +146,8 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/device',
+    'nuxt-mq',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -58,5 +165,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, {isClient}) {
+      if (isClient) {
+        config.optimization.splitChunks.maxSize = 51200
+      }
+    }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+  <section class="bg-main-page bg-cover h-screen w-screen flex flex-col items-start justify-center">
+    <span class="text-9xl w-4/6 px-20 text-white font-ametist">ВРЕМЯ КРАСОТЫ</span>
+    <span class="text-5xl w-3/6 px-20 text-white">МОДНАЯ ЦИРЮЛЬНЯ</span>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  head() {
+    return {
+      title: '«Время красоты» - Главная страница',
+      link: [
+        {rel: 'canonical', href: 'https://fashiontimekrd.ru/'},
+      ]
+    }
+  },
 }
 </script>
